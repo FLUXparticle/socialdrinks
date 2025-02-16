a70ea6003b0283f1603a6e3531e46b056996d232
Sven Reinck <sreinck@fluxparticle.de>
2025-02-16 06:49:56 +0100

    Validierung

--- socialdrinks-app/src/main/java/com/example/socialdrinks/app/controller/HelloController.java
+++ socialdrinks-app/src/main/java/com/example/socialdrinks/app/controller/HelloController.java
@@ -1,23 +1,52 @@
 package com.example.socialdrinks.app.controller;
 
+import jakarta.validation.constraints.*;
 import org.springframework.stereotype.*;
 import org.springframework.ui.*;
+import org.springframework.validation.*;
+import org.springframework.validation.annotation.*;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
 
     @GetMapping
-    public String hello() {
+    public String hello(Model model) {
+        model.addAttribute("form", new NameForm());
         return "hello";
     }
 
     @PostMapping
-    public String postHello(String name, Model model) {
-        model.addAttribute("name", name);
+    public String postHello(@Validated @ModelAttribute("form") NameForm form, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            List<String> errors = result.getAllErrors().stream()
+                    .map(ObjectError::getDefaultMessage)
+                    .toList();
 
+            model.addAttribute("errors", errors);
+
+            return "hello"; // Falls der Name leer ist, bleibt die Seite und zeigt den Fehler an
+        }
+
+        model.addAttribute("name", form.getName());
         return "hello";
+    }
+
+    public static class NameForm {
+
+        @NotBlank(message = "Name darf nicht leer sein.")
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 
 }
