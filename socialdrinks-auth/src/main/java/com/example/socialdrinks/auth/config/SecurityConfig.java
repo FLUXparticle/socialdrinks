d39bfdcca3d2ec14516141b073b5c16d3fcc4412
Sven Reinck <sreinck@fluxparticle.de>
2025-10-15 06:07:45 +0200

    CORS

--- socialdrinks-auth/src/main/java/com/example/socialdrinks/auth/config/SecurityConfig.java
+++ socialdrinks-auth/src/main/java/com/example/socialdrinks/auth/config/SecurityConfig.java
@@ -71,8 +71,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8082", "http://localhost:8083")); // Nur zum Testen
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedOrigins(List.of("http://localhost:8083"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
